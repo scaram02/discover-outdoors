9de89a44971d6522be94fb82b69dfb84762ddc96
Amelia Scarbrough <scaram02@Amelias-MacBook-Air.local>
2020-05-21 00:18:27 -0500

    nav dropdown

--- src/components/Mountain.js
+++ src/components/Mountain.js
@@ -7,7 +7,7 @@ export default class Mountain extends Component {
 
             <div class="high-container row text-center my-5">
              <div class="higlight col-md-6">
-             
+             <p>Actually it's probably a list/keys issue</p>
              <h2>{this.props.title}</h2>
             
              <h3>{this.props.descrip}</h3>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,10 +7,15 @@ export default class Nav extends Component {
             <div>
 <nav >
 <ul class="topnav">
-  <li><a href="default.asp">Home</a></li>
-  <li><a href="news.asp">Discover</a></li>
-  <li><a href="contact.asp">News</a></li>
-  <li><a href="about.asp">About</a></li>
+  <li><a href="d">Home</a></li>
+  <li class="dropdown">
+      <a href="">Discover</a>
+      <div class="dropdown-content">
+    <a href=''>Change later</a>
+  </div>
+      </li>
+  <li><a href="">News</a></li>
+  <li><a href="">About</a></li>
 </ul> 
 </nav>
             </div>
